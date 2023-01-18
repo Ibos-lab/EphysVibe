17b91c9b7397ff0b15faa3d310252d08e25ccec3
Guilhem <guilhem.ibos@univ-amu.fr>
2023-01-18 14:10:06 +0100

    add warnings and area selection

--- ephysvibe/pipelines/split_areas.py
+++ ephysvibe/pipelines/split_areas.py
@@ -25,8 +25,8 @@ def main(continuous_path):
     # Load continuous data
     continuous = recordnode.recordings[n_record].continuous[0]
     shape_0 = continuous.samples.shape[0]
-    areas = config.AREAS  # ["lip", "v4", "pfc", "eyes"]
-    n_channels = config.N_CHANNELS  # [32, 64, 64, 3]
+    areas = config.AREAS  
+    n_channels = config.N_CHANNELS  
     n_total = 0
     # Define dict to save info about channels
     channels_info = {"shape_0": shape_0, "areas": {}}
