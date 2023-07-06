7893074cf506c3db188c641e48980676556ac261
Camila Losada <camilosadaa@hotmail.com>
2023-07-06 09:45:29 +0200

    Add changes in the detection of sp in gp

--- ephysvibe/pipelines/generalized_phase.py
+++ ephysvibe/pipelines/generalized_phase.py
@@ -1,42 +1,17 @@
 import numpy as np
-
 import argparse
 from pathlib import Path
-from ..task import task_constants
-from ..structures.trials_data import TrialsData
-from ..analysis import signal
+from ..analysis import raw_ch, circular_stats
 import logging
 import os
-
-from ephysvibe.task import task_constants, def_task
-from ephysvibe.spike_sorting import utils_oe
-
-from numpy.core.umath import (
-    pi,
-    add,
-    arctan2,
-    frompyfunc,
-    cos,
-    less_equal,
-    sqrt,
-    sin,
-    mod,
-    exp,
-    not_equal,
-    subtract,
-)
+from numpy.core.umath import pi, mod
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 from scipy.signal import hilbert, butter, sosfilt, filtfilt
 
 from scipy.fft import fft, fftfreq, ifft
 import numpy.core.numeric as _nx
-from mne import time_frequency, create_info, EpochsArray
-from phylib.io.traces import get_ephys_reader
 
-import seaborn as sns
-
-from mne import time_frequency, create_info, EpochsArray
 
 logging.basicConfig(
     format="%(asctime)s | %(message)s ",
@@ -45,58 +20,33 @@ logging.basicConfig(
 )
 
 
-def load_continuous_dat(obj, **kwargs):
-    """load continuous.dat.
-    Parameters
-    ----------
-    obj : str or Path
-        Path to the raw data file.
-    sample_rate : float
-        The data sampling rate, in Hz.
-    n_channels_dat : int
-        The number of columns in the raw data file.
-    dtype : str
-        The NumPy data type of the raw binary file.
-    offset : int
-        The header offset in bytes.
-    """
-    kwargs = {
-        k: v
-        for k, v in kwargs.items()
-        if k in ("sample_rate", "n_channels_dat", "dtype", "offset")
-    }
-    traces = get_ephys_reader(obj, **kwargs)
-
-    return traces
-
-
 def main(
     path: Path,
     output_dir: Path,
 ):
     logging.info("--- Start ---")
-
     if not os.path.exists(path):
         raise FileExistsError
+    s_path = os.path.normpath(path).split(os.sep)
+    if "continuous.dat" != s_path[-1]:
+        logging.error("Path should end with /continuous.dat")
+        raise NameError
     # check if output dir exist, create it if not
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    s_path = os.path.normpath(path).split(os.sep)
-    ss_path = s_path[-1][:-3]
+    ss_path = s_path[-7]
+    samples_path = "/".join(s_path[:-1] + ["sample_numbers.npy"])
+    logging.info("Loading samples_numbers.py")
+    c_samples = np.load(samples_path)
 
-    continuous_path = "/envau/work/invibe/USERS/IBOS/openephys/Riesling/2023-03-17_10-11-51/Record Node 102/experiment1/recording1/continuous/Acquisition_Board-100.Rhythm Data/continuous.dat"
-    c_samples = np.load(
-        "/envau/work/invibe/USERS/IBOS/openephys/Riesling/2023-03-17_10-11-51/Record Node 102/experiment1/recording1/continuous/Acquisition_Board-100.Rhythm Data/sample_numbers.npy"
-    )
     shape_0 = len(c_samples)
     # Load data
-    logging.info("Loading, %s" % (ss_path))
-
     n_channels = 163
     sample_rate = 30000
     print(shape_0, n_channels)
     args = {"sample_rate": sample_rate, "n_channels_dat": n_channels, "dtype": "int16"}
-    cont = load_continuous_dat(continuous_path, **args)
+    logging.info("Loading %s" % (ss_path))
+    cont = raw_ch.load_continuous_dat(path, **args)
     # Parameters
     fs = 1000
     dt = 1 / fs
@@ -105,47 +55,48 @@ def main(
     hp_f = 5
     passband = [hp_f / (fs / 2), lp_f / (fs / 2)]
     # Compute GP
-    min_20 = 60 * 60 * 1000 * 30
-    min_50 = 100 * 60 * 1000 * 30
-    seg = np.arange(0, int((min_50 - min_20) / 30), 10000)
-    seg_phase_spikes = []
-    idx_ds = np.arange(0, 300000 - 10000, 30)
-    n_channels = 32
-    for i_seg in seg:
-        # seg_lfp,seg_sp = mat_select_seg(all_lfp,all_sp,i_seg)
-        # Select segment of the data
+    session_duration = (cont.shape[0] / 30000) / 60
+    if session_duration < 60:
+        logging.error("Session duration should be at least 60 min")
+        raise ValueError
 
-        # spike data
-        seg_hp = filter_continuous(
-            cont[i_seg : i_seg + 300000, :32], fs=30000, fc_hp=500, axis=0
+    minute_min = 20 * 60 * 1000 * 30
+    minute_max = minute_min + (20 * 60 * 1000 * 30)
+    raw_step = 300000
+    raw_1sec = 30000
+    seg = np.arange(0, int((minute_max - minute_min)), raw_step) + minute_min
+    seg_phase_spikes = []
+    idx_ds_sp = np.arange(0, raw_step + raw_1sec, 30)
+    idx_ds_lfp = np.arange(0, raw_step, 30)
+    n_channels = 32
+    logging.info("Computing gp")
+    for i_seg in seg:
+        # spike data = freq > 500 Hz
+        seg_hp = raw_ch.filter_continuous(
+            cont[i_seg : i_seg + raw_step + raw_1sec, :32], fs=30000, fc_hp=500, axis=0
         )  # High pass filter
         ## subsample
-        # subsample = idx_ds[i_seg:i_seg+10000].tolist()
-        seg_hp = seg_hp[idx_ds, :32].T
+        seg_hp = seg_hp[idx_ds_sp, :32].T
         ## detect spikes
-        seg_sp = detect_spikes(seg_hp)
+        seg_sp = raw_ch.detect_spikes(seg_hp)
 
-        # lfp data
-        seg_lp = filter_continuous(
-            cont[i_seg : i_seg + 300000, :n_channels], fs=30000, fc_lp=300, axis=0
+        seg_lp = raw_ch.filter_continuous(
+            cont[i_seg : i_seg + raw_step, :n_channels], fs=30000, fc_lp=300, axis=0
         )  # Low pass filter
-        b, a = butter(order, passband, "bandpass")
+        b, a = butter(4, passband, "bandpass")
         x_lfp = filtfilt(
             b, a, seg_lp, padtype="odd", padlen=3 * (max(len(b), len(a)) - 1), axis=0
         )
-        x_lfp = x_lfp[idx_ds, :n_channels].T
+        x_lfp = x_lfp[idx_ds_lfp, :n_channels].T
         # compute gp
-        xgp, wt = compute_generalized_phase(x_lfp, dt)
+        xgp, wt = circular_stats.compute_generalized_phase(x_lfp, dt)
         phase_lfp = np.angle(xgp.astype(complex))
 
         # Spike phases
         phase_spikes = []  # np.zeros(n_channels,n_channels)
-
         for lfp_ch in range(0, n_channels):
-
             ch_phase_spikes = []
             for sp_ch in range(0, n_channels):
-
                 if np.sum(seg_sp[sp_ch]) != 0:  # No spikes? No phase
                     ch_phase_spikes.append(
                         phase_lfp[lfp_ch, seg_sp[sp_ch].astype(bool)]
@@ -153,8 +104,8 @@ def main(
                 else:
                     ch_phase_spikes.append([np.nan])
             phase_spikes.append(ch_phase_spikes)
-
         seg_phase_spikes.append(phase_spikes)
+
     # Reorganise list
     ps = []
     for lfp_ch in range(0, n_channels):
@@ -169,18 +120,25 @@ def main(
     spike_phase = np.full((n_channels, n_channels), np.nan)
     pref_phase = np.full((n_channels, n_channels), np.nan)
     phase_corr = np.full((n_channels, n_channels), np.nan)
+    logging.info("Computing circular stats")
     for lfp_ch in range(0, n_channels):
         for sp_ch in range(0, n_channels):
             # phase_sp = np.concatenate(ps[lfp_ch][sp_ch])
             phase_sp = ps[lfp_ch][sp_ch][~np.isnan(ps[lfp_ch][sp_ch])]
-            spike_phase[lfp_ch, sp_ch] = resultant_vector_length(alpha=phase_sp)
-            pref_phase[lfp_ch, sp_ch] = mean_direction(alpha=phase_sp)
-            phase_corr[lfp_ch, sp_ch] = corrcc(phase_lfp[lfp_ch], phase_lfp[sp_ch])
+            spike_phase[lfp_ch, sp_ch] = circular_stats.resultant_vector_length(
+                alpha=phase_sp
+            )
+            pref_phase[lfp_ch, sp_ch] = circular_stats.mean_direction(alpha=phase_sp)
+            phase_corr[lfp_ch, sp_ch] = circular_stats.corrcc(
+                phase_lfp[lfp_ch], phase_lfp[sp_ch]
+            )
     # Plot and save phase_corr
     fig, ax = plt.subplots(figsize=(8, 5))
     sns.heatmap(phase_corr)
     ax.invert_yaxis()
     ax.invert_xaxis()
+    fig.suptitle("Phase correlation")
+    ax.set(xlabel="Channel number", ylabel="Channel number")
     logging.info("Saving phase_corr figure")
     fig.savefig(
         "/".join([os.path.normpath(output_dir)] + ["phase_corr_" + ss_path + ".jpg"]),
@@ -189,7 +147,13 @@ def main(
     plt.close(fig)
     # Plot and save pref_phase
     fig, ax = plt.subplots(figsize=(8, 5))
-    sns.heatmap(pref_phase.T, cmap="viridis")
+    sns.heatmap(
+        pref_phase.T,
+        cmap="RdBu_r",
+        cbar_kws={"label": "Preferred phase (rad)"},
+        vmin=-pi,
+        vmax=pi,
+    )
     ax.invert_yaxis()
     ax.invert_xaxis()
     logging.info("Saving pref_phase figure")
@@ -200,9 +164,11 @@ def main(
     plt.close(fig)
     # Plot and save
     fig, ax = plt.subplots(figsize=(8, 5))
-    sns.heatmap(spike_phase.T, cmap="hot")
+    sns.heatmap(spike_phase.T, cmap="hot", cbar_kws={"label": "SPI"})
     ax.invert_yaxis()
     ax.invert_xaxis()
+    fig.suptitle("Spike phase index (SPI)")
+    ax.set(xlabel="LFP channel", ylabel="MUA channel")
     logging.info("Saving spike_phase figure")
     fig.savefig(
         "/".join([os.path.normpath(output_dir)] + ["spike_phase_" + ss_path + ".jpg"]),
