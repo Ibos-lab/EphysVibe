232b1017a331c9ba36605e7562afeff4172e6a70
Camila Losada <camilosadaa@hotmail.com>
2023-12-11 15:38:04 +0100

    ~ Modify dockstring

--- ephysvibe/pipelines/preprocessing/compute_neurons.py
+++ ephysvibe/pipelines/preprocessing/compute_neurons.py
@@ -2,11 +2,7 @@ import argparse
 from pathlib import Path
 import logging
 import os
-from typing import List
 import numpy as np
-
-# from ...spike_sorting import utils_oe, config, data_structure
-import glob
 from ...structures.bhv_data import BhvData
 from ...structures.spike_data import SpikeData
 from ...structures.neuron_data import NeuronData
@@ -23,10 +19,10 @@ def main(
     sp_path: Path,
     output_dir: Path = "./output",
 ) -> None:
-    """Compute trials.
+    """Compute spikes by trial.
 
     Args:
-        sp_path (Path):  path to the continuous file (.dat) from OE.
+        sp_path (Path): path to the spike data file (sp.h5).
         output_dir (Path): output directory.
     """
     if not os.path.exists(sp_path):
@@ -38,9 +34,9 @@ def main(
     sp_path = os.path.normpath(sp_path)
     s_path = sp_path.split(os.sep)
     # load spike data
+    logging.info("Loading SpikeData")
     sp_data = SpikeData.from_python_hdf5(sp_path)
     # Select info about the recording from the path
-
     date_time = sp_data.date_time
     area = sp_data.area
     subject = sp_data.subject
@@ -50,16 +46,13 @@ def main(
     file_name = date_time + "_" + subject + "_e" + n_exp + "_r" + n_rec + "_bhv.h5"
     bhv_path = "/".join(s_path[:-3] + ["bhv"] + [file_name])
     bhv_path = os.path.normpath(bhv_path)
-
     # load bhv data
-    logging.info("Loading Spike and Bhv data")
+    logging.info("Loading Bhv data")
     bhv = BhvData.from_python_hdf5(bhv_path)
-
     # --------------------------
     code_samples = bhv.code_samples
     sp_samples = sp_data.sp_samples
     code_numbers = bhv.code_numbers
-    # --------------------------
     before_trial = 1000
     iti = 1500
     next_trial = 6000
@@ -67,9 +60,9 @@ def main(
         np.where(code_numbers == task_constants.EVENTS_B1["end_trial"], True, False)
     ]
     trials_start = code_samples[:, 0]
+    # --------------------------
     trials_max_duration = max(trials_end - trials_start)
     trials_max_duration = int(trials_max_duration + before_trial + iti + next_trial)
-
     n_trials = trials_start.shape[0]
     n_neurons = sp_samples.shape[0]
     tr_sp_data = np.full((n_trials, n_neurons, trials_max_duration), np.nan)
@@ -85,7 +78,6 @@ def main(
     code_samples_trial = code_samples - code_samples[:, 0].reshape(-1, 1) + before_trial
     i_neuron = 0
     i_mua = 0
-
     for i_n, cluster in enumerate(sp_data.clusters_group):
         if cluster == "good":
             i_neuron += 1
@@ -93,7 +85,7 @@ def main(
         elif cluster == "mua":
             i_mua += 1
             i_cluster = i_mua
-
+        # Define structure
         neuron_data = NeuronData(
             date_time=date_time,
             subject=subject,
@@ -121,7 +113,6 @@ def main(
         )
         output_d = os.path.normpath(output_dir)
         path = "/".join([output_d] + ["session_struct"] + [area] + ["neurons"])
-
         file_name = (
             date_time
             + "_"
@@ -137,6 +128,7 @@ def main(
             + str(i_cluster)
             + "_neu.h5"
         )
+        # Save neuron
         if not os.path.exists(path):
             os.makedirs(path)
         logging.info("Saving data")
